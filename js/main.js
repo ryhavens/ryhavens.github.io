896fc23c5816c9fbe7df8fdd6a9ec576002d94c8
Ryan Havens <ryan.d.havens@gmail.com>
2018-03-22 23:02:50 -0700

    Small update for 2018

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(function() {
+	"use strict";
+	// do nothing
+	console.log(`%c     ________________________________________
+	< mooooooooooooooooooooooooooooooooooooo >
+	 ----------------------------------------
+	        \\   ^__^
+	         \\  (oo)\\_______
+	            (__)\\       )\\/\\
+	                ||----w |
+	                ||     ||`, "font-family:monospace");
+	$(document).ready(function() {
+		$("#year").text(new Date().getFullYear());
+	});
+})();
